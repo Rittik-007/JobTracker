acbee538215c8833d7460204697f1f06fc14d074
Rittik-007 <munshirittik03@gmail.com>
2026-02-25 00:55:50 +0600

    Just some js stuff, closing for today due to late night.

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log('connected')
+let total = document.getElementById('total-count');
+
+let availableCount = document.getElementById('available-count');
+
+const allCards = document.getElementById('allcards');
+
+total.innerText = allCards.children.length; 
+availableCount.innerText = allCards.children.length + " jobs";
